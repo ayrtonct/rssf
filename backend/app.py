3845ec8383f143142992d9e161a93b1793bf8256
Ayrton César Teixeira e Silva <ayrton2004.acs@gmail.com>
2026-04-10 12:27:36 -0300

    fix: adiciona python-dotenv ao requirements

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 # app.py
+import os
 from flask import Flask
 from routes.medicoes import medicoes_bp
 
@@ -8,4 +9,5 @@ app = Flask(__name__)
 app.register_blueprint(medicoes_bp)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
